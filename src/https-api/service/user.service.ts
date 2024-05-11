b0635f9bac657b5deb00bb11a42f5b15e80c749f
Jez-er <andrey.chernenko008@gmail.com>
2024-05-11 21:40:50 +0300

    [ADD] HTTP User service

--- src/https-api/service/user.service.ts
+++ src/https-api/service/user.service.ts
@@ -1,10 +1,15 @@
 import {api} from "../index.ts";
 import {AxiosResponse} from "axios";
 import {IAllUser} from "../interface/user.interface.ts";
+import {IUserInfo} from "../interface/auth.interface.ts";
 
 class UserService {
     async allUsers(pageId: number): Promise<AxiosResponse<IAllUser>> {
         return api.get<IAllUser>(`/users/page/${pageId}`);
+    }
+
+    async userByUuid(uuid: string | undefined): Promise<AxiosResponse<IUserInfo>> {
+        return api.get<IUserInfo>(`users/uuid/${uuid}?assets=true`)
     }
 }
 
