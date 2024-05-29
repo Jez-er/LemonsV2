e669e8773338254bb32354a6f29f73b21a292e0f
Jez-er <andrey.chernenko008@gmail.com>
2024-05-29 22:44:46 +0300

    [ADD] Loeder

---
+++ src/components/ui/loader/index.tsx
@@ -0,0 +1,24 @@
+import style from './loaderStyle.module.scss'
+
+const Loader = () => {
+    return (<div className={style.container}>
+        <div className={style.block}></div>
+        <div className={style.block}></div>
+        <div className={style.block}></div>
+        <div className={style.block}></div>
+        <div className={style.block}></div>
+        <div className={style.block}></div>
+        <div className={style.block}></div>
+        <div className={style.block}></div>
+        <div className={style.block}></div>
+        <div className={style.block}></div>
+        <div className={style.block}></div>
+        <div className={style.block}></div>
+        <div className={style.block}></div>
+        <div className={style.block}></div>
+        <div className={style.block}></div>
+        <div className={style.block}></div>
+    </div>)
+}
+
+export default Loader
