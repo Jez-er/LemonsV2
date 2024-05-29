6e6c92a09c6b4f7fbcbe006e511a5cee84307a97
Jez-er <andrey.chernenko008@gmail.com>
2024-05-29 22:39:28 +0300

    [TEST]

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,6 +1,12 @@
 import { useNavigate } from "react-router-dom";
+import Modal from "../components/ui/Modal";
+import useModal from "../hooks/useModal.ts";
+import Loader from "../components/ui/loader";
+import ThemSwither from "../components/themSwither";
 
 const MainPage = () => {
+
+    const modalProps = useModal()
     const nav = useNavigate();
 
     const handleAuthClick = () => {
@@ -11,11 +17,25 @@ const MainPage = () => {
     };
 
     return (
-        <div>
+        <div className={'dark:bg-black min-h-screen'}>
+            <ThemSwither />
             <button onClick={handleAuthClick}>Авторизация</button>
             <button onClick={handleAdminClick}>Админка</button>
+            <button onClick={modalProps.onOpen}>Модалка</button>
+            <Modal {...modalProps}>
+                <Modal.Title>
+                    Тест
+                </Modal.Title>
+                <Modal.Body>
+                    Hello World
+                </Modal.Body>
+                <Modal.Footer>
+                    <button> Загрузить</button>
+                </Modal.Footer>
+            </ Modal>
+            <Loader />
         </div>
     );
 };
 
-export { MainPage };
+export {MainPage};
