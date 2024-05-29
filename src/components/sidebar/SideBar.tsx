8d6d4af94403dd39b29882ec501b2f59efd0ad51
Jez-er <andrey.chernenko008@gmail.com>
2024-05-29 22:37:50 +0300

    [UPDATE] Rewrite styles

--- src/components/sidebar/SideBar.tsx
+++ src/components/sidebar/SideBar.tsx
@@ -1,27 +1,26 @@
-import style from './SideBar.module.scss'
 import {UserAvatar} from "../ui/UserAvatar/UserAvatar.tsx";
 import {useUserStore} from "../../store/User.store.ts";
-import {Moon} from "lucide-react";
-import {AdminNavButton} from "../ui/AdminNavButton/AdminNavButton.tsx";
+import {AdminNavButton} from "./AdminNavButton/AdminNavButton.tsx";
+import ThemSwither from "../themSwither";
 
 const SideBar = () => {
 
     const User = useUserStore(state => state.user)
 
-    return (<div className={style.main}>
-        <h2 className={style.header}>LemonAdmin</h2>
-        <div className={style.user}>
-
+    return (<div className={'h-full w-full bg-white dark:bg-black dark:text-white flex flex-col'}>
+        <h2 className={'mx-auto text-orange-500 text-2xl font-bold mt-5'}>LemonAdmin</h2>
+        <div className={'flex mx-auto bg-gray dark:bg-dark-gray w-10/12 h-16 rounded-2xl p-4 items-center mt-5'}>
             <UserAvatar skin={User.assets.skin} />
-            <span style={{marginLeft: '0.7vw'}}>{User.username}</span>
-            <Moon strokeWidth={1}  style={{marginLeft: 'auto', marginRight: '1vw'}}/>
+            <span className={'ml-4'}>{User.username}</span>
+            <div className={'ml-auto mr-4'}>
+                <ThemSwither />
+            </div>
         </div>
-        <div className={style.category}>
-            <span className={style.categoryHeader}>Пользователи</span>
+        <div className={'mt-2.5'}>
+            <span className={'ml-3 opacity-45 font-medium'}>Пользователи</span>
             <AdminNavButton title={'Менеджер пользователей'} icon={'User'} to={'/admin/users/manager'} />
+            <AdminNavButton title={'Бан-Панель'} icon={'Ban'} to={'https://bans.lemonbrick.by/'} />
         </div>
-
-
         </div>)
 }
 
--- src/components/userlist/Userlist.tsx
+++ src/components/userlist/Userlist.tsx
@@ -1,4 +1,3 @@
-import style from './Userlist.module.scss'
 import {UserAvatar} from "../ui/UserAvatar/UserAvatar.tsx";
 import React, {FC} from "react";
 import {AllUsers} from "../../https-api/interface/user.interface.ts";
@@ -13,12 +12,13 @@ const Userlist: FC<UserlistProps> = ({data}) => {
     return (
         <>
             {data.map(user => (
-                <div className={style.main} key={user.id}>
-                    <span className={style.id}>{user.id}</span>
-                    <div className={style.logo}><UserAvatar skin={user.assets.skin}/></div>
-                    <span className={style.username}>{user.username}</span>
-                    <span className={style.uuid}>{user.uuid}</span>
-                    <NavLink className={style.btn} to={`/admin/users/manager/${user.uuid}`}>
+                <div className={'flex w-11/12 h-16 bg-white dark:bg-black dark:text-white mt-4 items-center rounded-xl'} key={user.id}>
+                    <span className={'ml-5 w-10'}>{user.id}</span>
+                    <div className={'w-3 h-3 bg-red ml-3 mr-7 rounded-xl'}></div>
+                    <div className={'ml-2'}><UserAvatar skin={user.assets.skin}/></div>
+                    <span className={'ml-5 w-40'}>{user.username}</span>
+                    <span className={'ml-3.5'}>{user.uuid}</span>
+                    <NavLink className={'bg-orange-500 ml-auto mr-10 w-10 h-10 flex justify-center items-center rounded-xl cursor-pointer'} to={`/admin/users/manager/${user.uuid}`}>
                         <Pen color={'White'} />
                     </NavLink>
                 </div>
