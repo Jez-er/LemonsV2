b66e56811fa7aa20a01f1852572c7954f7916044
Jez-er <andrey.chernenko008@gmail.com>
2024-05-29 22:31:36 +0300

    [UPDATE] AdminLayout

--- src/components/layouts/AdminLayout.tsx
+++ src/components/layouts/AdminLayout.tsx
@@ -1,13 +1,12 @@
-import style from './AdminLayout.module.scss'
 import {SideBar} from "../sidebar/SideBar.tsx";
 import {Outlet} from "react-router-dom";
 
 const AdminLayout = () => {
-    return (<div className={style.main}>
-        <div className={style.sidebar}>
+    return (<div className={'h-full w-full flex overflow-hidden'}>
+        <div className={'h-full w-1/5'}>
             <SideBar />
         </div>
-        <div className={style.outlet}>
+        <div className={'bg-gray dark:bg-dark-gray h-full w-full'}>
             <Outlet />
         </div>
     </div>)
